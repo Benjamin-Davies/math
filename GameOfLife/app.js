d1fc936e798c431233aee8970c5544839f4b7d55
Benjamin Davies <bentendavies@gmail.com>
2017-02-21 17:53:10 +1300

    Implemented multicolor

--- GameOfLife/app.js
+++ GameOfLife/app.js
@@ -5,7 +5,7 @@ function setup() {
   createCanvas(512, 512);
   frameRate(5);
   for (var i = 0; i < cols * rows; i++) {
-    grid[i] = round(random());
+    grid[i] = floor(random() * 2 + 1) * floor(random() * 2);
   }
 }
 
@@ -13,24 +13,29 @@ function draw() {
   var newGrid = [];
   for (var x = 0; x < cols; x++) {
     for (var y = 0; y < rows; y++) {
-      var count = 0;
+      var neighboors = 0;
+      var blue = 0, red = 0;
       
       for (var i = -1; i <= 1; i++) {
         for (var j = -1; j <= 1; j++) {
-          if (grid[ (x + i + (y + j) * cols) % (cols * rows) ] != 0)
-            count++;
+          var state = grid[ (x + i + (y + j) * cols) % (cols * rows) ];
+          if (i != 0 && j != 0 && state) {
+            neighboors++;
+            if (state == 1)
+              blue++;
+            else
+              red++;
+          }
         }
       }
 
       var state = grid[x + y * cols];
-      var newState = 0;
+      var newState = state;
 
-      if (state == 0) {
-        if (count == 3)
-          newState = 1;
-      } else {
-        if (count < 5 && count > 2)
-          newState = 1;
+      if (state && (neighboors < 2 || neighboors > 3))
+        newState = 0;
+      if (!state && neighboors == 3) {
+        newState = blue > red ? 1 : 2;
       }
 
       newGrid[x + y * cols] = newState;
