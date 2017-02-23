25cf7153961e12ba717cfb9b1557041c6d5e6f92
Benjamin Davies <bentendavies@gmail.com>
2017-02-23 17:35:26 +1300

    Fixed adjacents not counted as neighboors

--- GameOfLife/app.js
+++ GameOfLife/app.js
@@ -5,7 +5,16 @@ function setup() {
   createCanvas(512, 512);
   frameRate(5);
   for (var i = 0; i < cols * rows; i++) {
-    grid[i] = floor(random() * 2 + 1) * floor(random() * 2);
+    grid[i] = random() > 4/7 ? floor(random() * 2 + 1) : 0;
+  }
+}
+
+function render() {
+  for (var x = 0; x < cols; x++) {
+    for (var y = 0; y < rows; y++) {
+      fill(colors[grid[x + y * cols]]);
+      rect(x * 16, y * 16, 16, 16);
+    }
   }
 }
 
@@ -18,8 +27,8 @@ function draw() {
       
       for (var i = -1; i <= 1; i++) {
         for (var j = -1; j <= 1; j++) {
-          var state = grid[ (x + i + (y + j) * cols) % (cols * rows) ];
-          if (i != 0 && j != 0 && state) {
+          var state = grid[ (x + i + (y + j) * cols) ];
+          if (!(i == 0 && j == 0) && state) {
             neighboors++;
             if (state == 1)
               blue++;
@@ -32,7 +41,7 @@ function draw() {
       var state = grid[x + y * cols];
       var newState = state;
 
-      if (state && (neighboors < 2 || neighboors > 3))
+      if (state && !(neighboors == 2 || neighboors == 3))
         newState = 0;
       if (!state && neighboors == 3) {
         newState = blue > red ? 1 : 2;
@@ -43,10 +52,20 @@ function draw() {
   }
   grid = newGrid;
 
-  for (var x = 0; x < cols; x++) {
-    for (var y = 0; y < rows; y++) {
-      fill(colors[grid[x + y * cols]]);
-      rect(x * 16, y * 16, 16, 16);
-    }
+  render();
+}
+
+function mousePressed() {
+  if (mouseButton == "left" &&
+      mouseX > 0 && mouseX < width &&
+      mouseY > 0 && mouseY < height) {
+    var x = floor(mouseX / 16);
+    var y = floor(mouseY / 16);
+    if (grid[x + y * cols])
+      grid[x + y * cols] = 0;
+    else
+      grid[x + y * cols] = 1 + floor(2 * random());
+
+    render();
   }
 }
