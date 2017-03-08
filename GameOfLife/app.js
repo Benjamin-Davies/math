1f64e0ba1b390233fa907a83464dd5e82e4e21fa
Benjamin Davies <bentendavies@gmail.com>
2017-03-08 16:05:43 +1300

    Implemented game of life

--- GameOfLife/app.js
+++ GameOfLife/app.js
@@ -1,56 +1,68 @@
 var colors = ["black", "blue", "red"];
-var grid = [], cols = 32, rows = 32;
+var grid = [], cols = 64, rows = 32;
 
 function setup() {
-  createCanvas(512, 512);
+  createCanvas(1024, 512);
   frameRate(5);
+  initRandom();
+}
+
+function initRandom() {
   for (var i = 0; i < cols * rows; i++) {
-    grid[i] = random() > 4/7 ? floor(random() * 2 + 1) : 0;
+    grid[i] = random() > 1/2 ? floor(random() * 2 + 1) : 0;
   }
 }
 
+function step() {
+    var newGrid = [];
+    for (var x = 0; x < cols; x++) {
+      for (var y = 0; y < rows; y++) {
+        var neighboors = 0;
+        var blue = 0, red = 0;
+        
+        for (var i = -1; i <= 1; i++) {
+          for (var j = -1; j <= 1; j++) {
+            var state = grid[ ((x + i + cols) % cols + (y + j) * cols + (cols * rows)) % (cols * rows) ];
+            if (!(i == 0 && j == 0) && state) {
+              neighboors++;
+              if (state == 1)
+                blue++;
+              else
+                red++;
+            }
+          }
+        }
+
+        var state = grid[x + y * cols];
+        var newState = state;
+
+        if (state && !(neighboors == 2 || neighboors == 3))
+          newState = 0;
+        if (!state && neighboors == 3) {
+          newState = blue > red ? 1 : 2;
+        }
+
+        newGrid[x + y * cols] = newState;
+      }
+    }
+    grid = newGrid;
+  }
+
 function render() {
+  stroke(colors[0]);
   for (var x = 0; x < cols; x++) {
     for (var y = 0; y < rows; y++) {
-      fill(colors[grid[x + y * cols]]);
+      fill(colors[grid[x + y * cols]] || colors[0]);
       rect(x * 16, y * 16, 16, 16);
     }
   }
 }
 
+var paused;
 function draw() {
-  var newGrid = [];
-  for (var x = 0; x < cols; x++) {
-    for (var y = 0; y < rows; y++) {
-      var neighboors = 0;
-      var blue = 0, red = 0;
-      
-      for (var i = -1; i <= 1; i++) {
-        for (var j = -1; j <= 1; j++) {
-          var state = grid[ (x + i + (y + j) * cols) % (cols * rows) ];
-          if (!(i == 0 && j == 0) && state) {
-            neighboors++;
-            if (state == 1)
-              blue++;
-            else
-              red++;
-          }
-        }
-      }
-
-      var state = grid[x + y * cols];
-      var newState = state;
-
-      if (state && !(neighboors == 2 || neighboors == 3))
-        newState = 0;
-      if (!state && neighboors == 3) {
-        newState = blue > red ? 1 : 2;
-      }
-
-      newGrid[x + y * cols] = newState;
-    }
+  if (!paused) {
+    step();
   }
-  grid = newGrid;
 
   render();
 }
--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Math
 This repository contains various projects that are based on maths concepts.
 
+### [Conway's Game of Life](./GameOfLife/)
+This is the traditional Game of Life... with a twist. There are two different colours and when a cell is born it recieves the color of the majority of it's parents. There will always be a dominant colour because a in order to be born a cell must have three live neighboors and three is odd. Once it is given a color, the cell keeps its colour until it dies.
+
 ### [Restosphene's Sieve](./RestosphenesSieve/)
 A Resptosphene's Sieve is a grid where you systematicaly cross out all of the composite numbers and you are left with all of the primes. Usualy the top left corner is one but I made it be zero because it made it easier to implement. Also there are no numbers drawn; I only filled in the squares.
