e40c1d35ddd243988c6f920f2586ccb15c121d75
Benjamin-Davies <bentendavies@gmail.com>
2020-08-01 14:16:52 +1200

    Add build script

--- ML/build.js
+++ ML/build.js
@@ -1,6 +1,6 @@
-const Bundler = require('parcel');
 const path = require('path');
 const fs = require('fs');
+const child_process = require('child_process');
 
 const isDev = process.argv.includes('--dev');
 
@@ -18,17 +18,30 @@ fs
     fs.rmdirSync(dir);
   });
 
-const options = { watch: isDev };
+// const options = { watch: isDev };
 const publicUrl = isDev ? '/' : '/math/ML/';
 const srcDir = path.join(__dirname, 'src/');
+const distDir = path.join(__dirname, 'dist/');
 
 fs.readdirSync(srcDir).forEach(dir => {
-  new Bundler(path.join(srcDir, dir, 'index.html'), {
-    ...options,
-    outDir: path.join(__dirname, dir),
-    publicUrl: publicUrl + dir
-  }).bundle();
+  // new Bundler(path.join(srcDir, dir, 'index.html'), {
+  //   ...options,
+  //   outDir: path.join(distDir, dir),
+  //   publicUrl: publicUrl + dir
+  // }).bundle();
+  child_process.execSync(
+    `npx parcel build ${path.join(
+      srcDir,
+      dir,
+      'index.html'
+    )} --out-dir ${path.join(distDir, dir)} --public-url ${publicUrl + dir}`,
+    { stdio: 'inherit' }
+  );
 });
+
+for (const file of ['index.html', 'style.css']) {
+  fs.copyFileSync(path.join(__dirname, file), path.join(distDir, file));
+}
 
 if (isDev) {
   const express = require('express');
--- utils.ts
+++ utils.ts
@@ -1,14 +1,23 @@
-import * as fs from 'fs';
+import { existsSync, ensureDir, emptyDir, readdirSync, copy, readJson, remove } from 'fs-extra';
 import { join } from 'path';
-import { execSync } from 'child_process'
+import { execSync } from 'child_process';
 
-const excludedDirs = ['node_modules', '.git', 'build'];
-const files = fs
-  .readdirSync(__dirname)
-  .filter((file) => !excludedDirs.includes(file));
+const distDir = join(__dirname, 'dist');
+
+const excludedFiles = [
+  '.git',
+  '.vscode',
+  'dist',
+  'node_modules',
+  'package.json',
+  'package-lock.json',
+];
+const files = readdirSync(__dirname).filter(
+  (file) => !excludedFiles.includes(file)
+);
 
 const npmDirs = files.filter((file) =>
-  fs.existsSync(join(__dirname, file, 'package.json'))
+  existsSync(join(__dirname, file, 'package.json'))
 );
 
 const commands = {
@@ -18,6 +27,39 @@ const commands = {
         cwd: join(__dirname, dir),
         stdio: 'inherit',
       });
+    }
+  },
+  async build() {
+    await ensureDir(distDir);
+    await emptyDir(distDir);
+
+    for (const file of files) {
+      if (!npmDirs.includes(file)) {
+        await copy(join(__dirname, file), join(distDir, file), {
+          recursive: true,
+        });
+      }
+    }
+
+    for (const dir of npmDirs) {
+      const packageJson = await readJson(join(__dirname, dir, 'package.json'));
+      if (packageJson.scripts.hasOwnProperty('build')) {
+        await remove(join(__dirname, dir, 'dist'));
+        await remove(join(__dirname, dir, '.cache'));
+
+        execSync('npm run build', {
+          cwd: join(__dirname, dir),
+          stdio: 'inherit',
+        });
+
+        await copy(join(__dirname, dir, 'dist'), join(distDir, dir), {
+          recursive: true,
+        });
+      } else {
+        console.warn(
+          `Package ${packageJson.name} (${dir}/) has no build script. Skipping...`
+        );
+      }
     }
   },
 };
