4a0fe23abe70f309d59d214bea90c4dad06e81cd
Benjamin-Davies <bentendavies@gmail.com>
2020-08-01 15:33:52 +1200

    Add README generator

--- utils.ts
+++ utils.ts
@@ -1,6 +1,18 @@
-import { existsSync, ensureDir, emptyDir, readdirSync, copy, readJson, remove } from 'fs-extra';
+import {
+  existsSync,
+  ensureDir,
+  emptyDir,
+  readdirSync,
+  copy,
+  readJson,
+  remove,
+  appendFile,
+  readFile,
+} from 'fs-extra';
 import { join } from 'path';
 import { execSync } from 'child_process';
+import { noCase } from 'change-case';
+import { titleCase } from "title-case";
 
 const distDir = join(__dirname, 'dist');
 
@@ -33,6 +45,7 @@ const commands = {
     await ensureDir(distDir);
     await emptyDir(distDir);
 
+    // Copy non-npm files
     for (const file of files) {
       if (!npmDirs.includes(file)) {
         await copy(join(__dirname, file), join(distDir, file), {
@@ -41,25 +54,50 @@ const commands = {
       }
     }
 
+    // Build npm projects and copy results
     for (const dir of npmDirs) {
       const packageJson = await readJson(join(__dirname, dir, 'package.json'));
       if (packageJson.scripts.hasOwnProperty('build')) {
         await remove(join(__dirname, dir, 'dist'));
         await remove(join(__dirname, dir, '.cache'));
 
-        execSync('npm run build', {
-          cwd: join(__dirname, dir),
-          stdio: 'inherit',
-        });
+        // execSync('npm run build', {
+        //   cwd: join(__dirname, dir),
+        //   stdio: 'inherit',
+        // });
 
-        await copy(join(__dirname, dir, 'dist'), join(distDir, dir), {
-          recursive: true,
-        });
+        // await copy(join(__dirname, dir, 'dist'), join(distDir, dir), {
+        //   recursive: true,
+        // });
       } else {
         console.warn(
           `Package ${packageJson.name} (${dir}/) has no build script. Skipping...`
         );
       }
+    }
+
+    // Compile the readmes into one
+    const readmes: { name: string; content: string; commitDate: Date }[] = [];
+    for (const name of files) {
+      if (existsSync(join(__dirname, name, 'README.md'))) {
+        const content = await readFile(join(__dirname, name, 'README.md'), {
+          encoding: 'utf8',
+        });
+        const commitDate = execSync(
+          `git log -n 1 --pretty=format:%ci ${join(__dirname, name)}`,
+          {
+            encoding: 'utf8',
+          }
+        );
+        readmes.push({ name, content, commitDate: new Date(commitDate) });
+      }
+    }
+    readmes.sort((a, b) => (a.commitDate > b.commitDate ? 1 : -1));
+    for (const { name, content } of readmes) {
+      await appendFile(
+        join(distDir, 'README.md'),
+        `\n### [${titleCase(noCase(name))}](./${name}/)\n\n${content}`
+      );
     }
   },
 };
