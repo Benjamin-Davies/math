da549c09e0d41decac91c75b78ef12848b9c8ad6
Benjamin-Davies <bentendavies@gmail.com>
2020-09-24 13:53:45 +1200

    Fix proxy paths

--- ML/build.js
+++ ML/build.js
@@ -32,7 +32,7 @@ for (const file of ['index.html', 'style.css']) {
 if (isDev) {
   const express = require('express');
   const app = express();
-  app.use(express.static(__dirname + '/dist'));
+  app.use(publicUrl, express.static(__dirname + '/dist'));
   app.listen(process.env.PORT || 8080, () => {
     console.log(`Dev server running at http://localhost:${process.env.PORT || 8080}/`);
   });
