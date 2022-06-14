c86722accf3242c43be4575b5a67dc522a15004a
Benjamin-Davies <bentendavies@gmail.com>
2022-06-14 00:53:54 +0000

    Update gh-pages to output generated at 5a3dfba

---
+++ FourierCircles/app.js
@@ -0,0 +1,78 @@
+import { clear, drawCircles, drawLines } from './draw.js';
+
+const N = 10;
+
+let time = 0;
+/**@type {[number, number][]} */
+const points = [];
+
+/**@type {[number, number]} */
+let origin = [250, 250];
+/**@type {[number, number][]} */
+let circles = [
+  [20, 50],
+  [100, 0],
+  [50, 100],
+];
+
+document.addEventListener('mousedown', (ev) => {
+  points.push([ev.clientX, ev.clientY]);
+  calculateCircles();
+});
+
+document.addEventListener('touchstart', (ev) => {
+  points.push([ev.targetTouches[0].clientX, ev.targetTouches[0].clientY]);
+  calculateCircles();
+});
+
+/**
+ * Convert an index to alternating positive negative integers
+ * @param {number} i
+ */
+export function iToN(i) {
+  if (i % 2 === 0) {
+    return i / 2 + 1;
+  } else {
+    return -(i + 1) / 2;
+  }
+}
+
+function calculateCircles() {
+  const [x_total, y_total] = points.reduce(([x_1, y_1], [x_2, y_2]) => [
+    x_1 + x_2,
+    y_1 + y_2,
+  ]);
+  origin = [x_total / points.length, y_total / points.length];
+
+  circles = Array(Math.min(points.length - 1, 2 * N))
+    .fill(0)
+    .map((_, i) => {
+      const n = iToN(i);
+      const [a_total, b_total] = points
+        .map(([x, y], i) => {
+          x -= origin[0];
+          y -= origin[1];
+          const theta = (2 * n * Math.PI * i) / points.length;
+          return [
+            x * Math.cos(theta) + y * Math.sin(theta),
+            -x * Math.sin(theta) + y * Math.cos(theta),
+          ];
+        })
+        .reduce(([a_1, b_1], [a_2, b_2]) => [a_1 + a_2, b_1 + b_2]);
+      return [a_total / points.length, b_total / points.length];
+    });
+}
+
+function draw() {
+  clear();
+  drawLines(points);
+  drawCircles(origin, circles, time);
+
+  time += 0.01;
+  time %= 2 * Math.PI;
+  // time = 2 * Math.PI;
+
+  requestAnimationFrame(draw);
+}
+
+draw();
---
+++ FourierCircles/draw.js
@@ -0,0 +1,81 @@
+import { iToN } from './app.js';
+
+const DELTA_T = 0.01;
+
+const canvas = document.createElement('canvas');
+const width = (canvas.width = window.innerWidth);
+const height = (canvas.height = window.innerHeight);
+document.body.appendChild(canvas);
+
+const ctx = canvas.getContext('2d');
+
+export function clear() {
+  ctx.fillStyle = '#eee';
+  ctx.fillRect(0, 0, width, height);
+}
+
+/**
+ * @param {[number, number][]} points Array of [x, y] pairs
+ */
+export function drawLines(points) {
+  ctx.strokeStyle = '#00f3';
+  ctx.lineWidth = 2;
+  ctx.beginPath();
+  for (const [x, y] of points) {
+    ctx.lineTo(x, y);
+  }
+  ctx.closePath();
+  ctx.stroke();
+}
+
+/**
+ * @param {[number, number]} origin a_0
+ * @param {[number, number][]} circles Array of [a_n, b_n] pairs
+ * @param {number} time Draw curve where 0 <= t < time
+ */
+export function drawCircles(origin, circles, time) {
+  ctx.strokeStyle = '#000a';
+  ctx.lineWidth = 2;
+  ctx.beginPath();
+  for (let t = 0; t < time; t += DELTA_T) {
+    let [x, y] = origin;
+    let i = 0;
+    for (const [a_n, b_n] of circles) {
+      const n = iToN(i);
+      [x, y] = [
+        x + a_n * Math.cos(n * t) - b_n * Math.sin(n * t),
+        y + a_n * Math.sin(n * t) + b_n * Math.cos(n * t),
+      ];
+      i++;
+    }
+    ctx.lineTo(x, y);
+  }
+  ctx.stroke();
+
+  const rPhiCircles = circles.map(([a_n, b_n]) => [
+    Math.sqrt(a_n * a_n + b_n * b_n),
+    Math.atan2(b_n, a_n),
+  ]);
+
+  ctx.fillStyle = '#0001';
+  ctx.strokeStyle = '#0003';
+  ctx.lineWidth = 2;
+  let [x, y] = origin;
+  let i = 0;
+  for (const [r, phi] of rPhiCircles) {
+    ctx.beginPath();
+    ctx.ellipse(x, y, r, r, 0, 0, 2 * Math.PI);
+    ctx.stroke();
+
+    const n = iToN(i);
+    [x, y] = [
+      x + r * Math.cos(phi + n * time),
+      y + r * Math.sin(phi + n * time),
+    ];
+    i++;
+
+    ctx.beginPath();
+    ctx.ellipse(x, y, 5, 5, 0, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+}
