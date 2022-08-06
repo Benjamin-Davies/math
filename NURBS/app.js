a4fa8f716c4bdacf5114002ea601cb6a6d3a91d3
Benjamin Davies <bentendavies@gmail.com>
2022-08-06 17:38:10 +1200

    Try spline

--- NURBS/app.js
+++ NURBS/app.js
@@ -7,21 +7,42 @@ ctx.fillStyle = 'black';
 
 const points = [
   [0, 0],
-  [400, 100],
-  [400, 200],
-  [000, 400],
+  [50, 300],
+  [250, 250],
+  [300, 400],
+  [400, 0],
+  [250, 50],
 ];
 
+const t = points.map((_, i) => i / (points.length - 1));
+
+const N = 1;
+
 ctx.beginPath();
-for (let t = 0; t < 1; t += 0.01) {
-  const u = 1 - t;
-  const a = t * t * t;
-  const b = 3 * t * t * u;
-  const c = 3 * t * u * u;
-  const d = u * u * u;
-  ctx.lineTo(
-    a * points[0][0] + b * points[1][0] + c * points[2][0] + d * points[3][0],
-    a * points[0][1] + b * points[1][1] + c * points[2][1] + d * points[3][1]
-  );
+for (const [x, y] of points) {
+  ctx.lineTo(x, y);
+}
+ctx.stroke();
+
+function add(a, b) {
+  return a + b;
+}
+
+function B(x, i, n) {
+  if (n === 0) return t[i] <= x && x < t[i + 1] ? 1 : 0;
+
+  const k = n - 1;
+  const omega = (i) => (x - t[i]) / (t[i + k] - t[i]);
+  return omega(i) * B(x, i, k) + (1 - omega(i + 1)) * B(x, i + 1, k);
+}
+
+ctx.beginPath();
+for (let t = 0; t <= 1; t += 0.1) {
+  const [x, y] = [
+    points.map((p, i) => p[0] * B(t, i, N)).reduce(add),
+    points.map((p, i) => p[1] * B(t, i, N)).reduce(add),
+  ];
+  console.log(t, x, y);
+  ctx.lineTo(x, y);
 }
 ctx.stroke();
