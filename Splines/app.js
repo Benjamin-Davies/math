ee25c0b8d0e341479b7e7d0173916405746d34b8
Benjamin-Davies <bentendavies@gmail.com>
2022-12-15 02:45:45 +0000

    Update gh-pages to output generated at c8897c7

--- Splines/app.js
+++ Splines/app.js
@@ -2,7 +2,7 @@ const WIDTH = 400;
 const HEIGHT = 400;
 const DELTA_T = 1e-3;
 
-const shapes = ['circle', 'parabola'];
+const shapes = ['circle', 'parabola', 'zig-zag'];
 let shape = shapes[0];
 
 const canvas = document.createElement('canvas');
@@ -15,21 +15,11 @@ const ctx = canvas.getContext('2d');
 /**
  * @param {number} degree
  * @param {number[][]} controlPoints
+ * @param {number[]} weights
  * @param {number[]} knotVector
  */
-function drawSpline(degree, controlPoints, knotVector) {
+function drawSpline(degree, controlPoints, weights, knotVector) {
   const lastKnot = knotVector[knotVector.length - 1];
-  let knotIndex = 0;
-
-  const applyBases = (
-    /** @type {number[]} */ bases,
-    /** @type {number} */ offset,
-    /** @type {number} */ component
-  ) =>
-    bases.reduce(
-      (acc, basis, i) => acc + basis * controlPoints[offset + i][component],
-      0
-    );
 
   const f = (
     /** @type {number} */ i,
@@ -59,9 +49,30 @@ function drawSpline(degree, controlPoints, knotVector) {
     return f(i, n, u) * N(i, n - 1, u) + g(i + 1, n, u) * N(i + 1, n - 1, u);
   };
 
+  const applyBases = (
+    /** @type {number[]} */ bases,
+    /** @type {number} */ offset,
+    /** @type {number} */ component
+  ) => {
+    const numerator = bases.reduce((acc, basis, j) => {
+      const i = offset + j;
+      const w_i = weights[i];
+      const P_i = controlPoints[i][component];
+      return acc + basis * w_i * P_i;
+    }, 0);
+    const denominator = bases.reduce((acc, basis, j) => {
+      const i = offset + j;
+      const w_i = weights[i];
+      return acc + basis * w_i;
+    }, 0);
+    return numerator / denominator;
+  };
+
+  let segment = 0;
   for (let u = 0; u < lastKnot; u += DELTA_T) {
-    while (knotVector[knotIndex + 1] <= u) knotIndex++;
-    const offset = knotIndex - degree;
+    while (knotVector[segment + 1] <= u) segment++;
+    // Index of the first control point that is active (starts out negative)
+    const offset = segment - degree;
 
     ctx.fillStyle = `hsl(${u * 360}deg, 95%, 40%)`;
 
@@ -72,8 +83,7 @@ function drawSpline(degree, controlPoints, knotVector) {
     ];
     const x = applyBases(bases, offset, 0);
     const y = applyBases(bases, offset, 1);
-    const w = applyBases(bases, offset, 2);
-    ctx.fillRect(x / w, y / w, 1, 1);
+    ctx.fillRect(x, y, 1, 1);
   }
 }
 
@@ -83,40 +93,63 @@ function draw() {
   ctx.translate(WIDTH / 2, HEIGHT / 2);
   ctx.scale(1, -1);
 
-  let controlPoints, knotVector;
+  let controlPoints, weights, knotVector;
   switch (shape) {
     case 'circle':
       controlPoints = [
-        [100, 0, 1],
-        [100 * Math.SQRT1_2, 100 * Math.SQRT1_2, Math.SQRT1_2],
-        [0, 100, 1],
-        [-100 * Math.SQRT1_2, 100 * Math.SQRT1_2, Math.SQRT1_2],
-        [-100, 0, 1],
-        [-100 * Math.SQRT1_2, -100 * Math.SQRT1_2, Math.SQRT1_2],
-        [0, -100, 1],
-        [100 * Math.SQRT1_2, -100 * Math.SQRT1_2, Math.SQRT1_2],
-        [100, 0, 1],
+        [100, 0],
+        [100, 100],
+        [0, 100],
+        [-100, 100],
+        [-100, 0],
+        [-100, -100],
+        [0, -100],
+        [100, -100],
+        [100, 0],
+      ];
+      weights = [
+        1,
+        Math.SQRT1_2,
+        1,
+        Math.SQRT1_2,
+        1,
+        Math.SQRT1_2,
+        1,
+        Math.SQRT1_2,
+        1,
       ];
       knotVector = [0, 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 4];
       break;
 
     case 'parabola':
       controlPoints = [
-        [100, 100, 1],
-        [0, -100, 1],
-        [-100, 100, 1],
+        [-100, 100],
+        [0, -100],
+        [100, 100],
       ];
+      weights = [1, 1, 1];
       knotVector = [0, 0, 0, 1, 1, 1];
+      break;
+
+    case 'zig-zag':
+      controlPoints = [
+        [-100, 100],
+        [100, 100],
+        [-100, -100],
+        [100, -100],
+      ];
+      weights = [1, 4, 8, 1];
+      knotVector = [0, 0, 0, 1, 2, 2, 2];
       break;
   }
 
   ctx.fillStyle = '#e77';
-  drawSpline(2, controlPoints, knotVector);
+  drawSpline(2, controlPoints, weights, knotVector);
 
   ctx.fillStyle = '#77e';
-  for (const [x, y, w] of controlPoints) {
+  for (const [x, y] of controlPoints) {
     ctx.beginPath();
-    ctx.arc(x / w, y / w, 3, 0, 2 * Math.PI);
+    ctx.arc(x, y, 3, 0, 2 * Math.PI);
     ctx.fill();
   }
 
@@ -132,6 +165,7 @@ for (const shape of shapes) {
   option.value = shape;
   select.appendChild(option);
 }
+select.value = shape;
 select.addEventListener('input', () => {
   shape = select.value;
   draw();
